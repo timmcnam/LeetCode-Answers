3397edf1f38c2da36483909850426af12b26b71f
Timothy McNamara <timmymcnamara83@gmail.com>
2024-07-10 17:37:24 -0400

    Final Code with comments

--- Easy/28_Find_the_Index_of_the_First_Occurrence_in_a_String.c
+++ Easy/28_Find_the_Index_of_the_First_Occurrence_in_a_String.c
@@ -1,5 +1,7 @@
 /*
-* Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+* Given two strings needle and haystack, 
+* return the index of the first occurrence of needle in haystack, 
+* or -1 if needle is not part of haystack.
 * 
 * Example 1:
 *
@@ -20,3 +22,52 @@
 * haystack and needle consist of only lowercase English characters.
 * 
 */
+
+int strStr(char* haystack, char* needle) {
+    // if null, return 0
+   if (*needle == '\0') {
+        return 0;
+    }
+
+    int index = 0;
+
+    while (*haystack) {
+        // takes the current position of haystack
+        char *z = haystack;
+        // takes the beginning string character of needle
+        char *t = needle;
+
+        // while we are pointing to a character and the characters 
+        // we are pointing to are equal to each other (potential match found code)
+        while (*z && *t && (*z == *t)) {
+            z++;
+            t++;
+        }
+
+        // If we reached the end of needle, we found a match
+        if (*t == '\0') {
+            return index;
+        }
+
+        // if none of the conditions above are met, 
+        // keep incrementing haystack till we find a letter
+        // beginning with the same one as needle
+        haystack++;
+        // increment until we find where the needle starts
+        index++;
+    }
+
+    // If no match is found, return -1
+    return -1;
+}
+
+
+// Code using Code::Blocks to print the returned index from main
+int main()
+{
+    char haystack[] = "nutterbutter";
+    char needle[] = "butter";
+    int result = strStr(haystack, needle);
+    printf("%d\n", result);
+    return 0;
+}
